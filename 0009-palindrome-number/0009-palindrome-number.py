17e2302bafc1eb08d4be9e3c247be62e31db02ea
Muhammad-Kashif-javed <155528616+Muhammad-Kashif-javed@users.noreply.github.com>
2025-04-28 17:16:34 +0500

    Update 0009-palindrome-number.py

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -14,5 +14,17 @@ class Solution:
         else:
             return False
             
+'''Time Complexity:
 
-       
+The key operation happens inside the while num > 0: loop.
+In each iteration, you divide num by 10 (num = num // 10).
+Therefore, the number of iterations is proportional to the number of digits in x.
+The number of digits in a number x is approximately log₁₀(x).
+(Because if x has d digits, then 10^(d-1) <= x < 10^d.)
+Thus, the time complexity is:𝑂(log10(𝑥)) or O(log(x))
+​Space Complexity:
+You use a few integer variables (num, revnum, last_digit), but no extra space that grows with input size.
+Thus, space complexity is: 𝑂(1)'''
+
+​
+
